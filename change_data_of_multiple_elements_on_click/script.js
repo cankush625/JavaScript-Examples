0713c05414fd61ca718124df8f56231764d25eaf
cankush625 <cankush625@gmail.com>
2021-10-24 21:59:31 +0530

    refactor(Clean Code): Clean the code and add comments
    Signed-off-by: cankush625 <cankush625@gmail.com>

--- change_data_of_multiple_elements_on_click/script.js
+++ change_data_of_multiple_elements_on_click/script.js
@@ -1,20 +1,17 @@
-function getName(name) {
-    console.log("Hello " + name);
-}
-
-getName();
-getName("Ankush");
-
-let onElementClick = (className) => {
-    className.onclick = _ => {
-        className.innerHTML = "JavaScript";
+// Function to change the inner HTML of the HTML element on click
+let onElementClick = (htmlElement) => {
+    htmlElement.onclick = _ => {
+        htmlElement.innerHTML = "JavaScript";
         console.log("Im CLicked!");
     }
 };
 
+// Get all elements with a class name "js"
 const span = document.getElementsByClassName("js");
 
 let element;
+// Iterate through every element in the span variable and
+// run the onElementClick function for them
 for (let i = 0; i < span.length; i++) {
     element = span[i];
     onElementClick(element);
